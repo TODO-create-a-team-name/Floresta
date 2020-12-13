8cd6b7788e34aefa758b2482ea2363edb9d2f3d5
Serge <sanichace@gmail.com>
2020-12-13 22:49:12 +0200

    Added RolesController with views Added two buttons for admins: Manage users Manage roles

---
+++ Floresta/Controllers/RolesController.cs
@@ -0,0 +1,98 @@
+﻿using Floresta.Models;
+using Floresta.ViewModels;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Floresta.Controllers
+{
+    [Authorize(Roles = "admin")]
+    public class RolesController : Controller
+    {
+        RoleManager<IdentityRole> _roleManager;
+        UserManager<User> _userManager;
+        public RolesController(RoleManager<IdentityRole> roleManager, UserManager<User> userManager)
+        {
+            _roleManager = roleManager;
+            _userManager = userManager;
+        }
+
+        public IActionResult Index() => View(_roleManager.Roles.ToList());
+
+        public IActionResult Create() => View();
+
+        [HttpPost]
+        public async Task<IActionResult> Create(string name)
+        {
+            if (!string.IsNullOrEmpty(name))
+            {
+                IdentityResult result = await _roleManager.CreateAsync(new IdentityRole(name));
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    foreach (var error in result.Errors)
+                    {
+                        ModelState.AddModelError(string.Empty, error.Description);
+                    }
+                }
+            }
+            return View(name);
+        }
+
+        public IActionResult UserList() => View(_userManager.Users.ToList());
+
+        public async Task<IActionResult> Edit(string userId)
+        {
+            // getting user
+            User user = await _userManager.FindByIdAsync(userId);
+            if (user != null)
+            {
+                // getting the list of roles of the user
+                var userRoles = await _userManager.GetRolesAsync(user);
+                var allRoles = _roleManager.Roles.ToList();
+                ChangeRoleViewModel model = new ChangeRoleViewModel
+                {
+                    UserId = user.Id,
+                    UserEmail = user.Email,
+                    UserRoles = userRoles,
+                    AllRoles = allRoles
+                };
+                return View(model);
+            }
+
+            return NotFound();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(string userId, List<string> roles)
+        {
+            // getting user
+            User user = await _userManager.FindByIdAsync(userId);
+            if (user != null)
+            {
+                // getting the list of roles of the user
+                var userRoles = await _userManager.GetRolesAsync(user);
+                // getting all roles
+                var allRoles = _roleManager.Roles.ToList();
+                // getting the list of roles which were added
+                var addedRoles = roles.Except(userRoles);
+                // getting the list of roles which were deleted
+                var removedRoles = userRoles.Except(roles);
+
+                await _userManager.AddToRolesAsync(user, addedRoles);
+
+                await _userManager.RemoveFromRolesAsync(user, removedRoles);
+
+                return RedirectToAction("UserList");
+            }
+
+            return NotFound();
+        }
+    }
+}
--- Floresta/Controllers/UsersController.cs
+++ Floresta/Controllers/UsersController.cs
@@ -8,7 +8,7 @@ using System.Threading.Tasks;
 
 namespace Floresta.Controllers
 {
-    //[Authorize(Roles = "admin")]
+    [Authorize(Roles = "admin")]
     public class UsersController : Controller
     {
         UserManager<User> _userManager;
