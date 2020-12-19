2ecc58ac4ce0b93153a91d3a32c37d6c4267b48a
Demianchuk <vanohahj5555@gmail.com>
2020-12-19 21:54:56 +0200

    Added animation for menu in main page, implemented design for login page(not yet done).

--- Floresta/wwwroot/js/site.js
+++ Floresta/wwwroot/js/site.js
@@ -1,15 +1,25 @@
 ﻿
     var menu = document.querySelector('.nav_menu'),
         icon = document.querySelector('.animated_menu_icon'),
-        menuButton = document.querySelector('#menu_button_container');
+        menuButton = document.querySelector('#menu_button_container'),
+        open= false;
 
-    menuButton.onclick = function () {
-        if (menu.style.display !== 'none') {
-            menu.classList.toggle('menu-opened');
-            icon.classList.toggle('menu-opened');
-        }
-        else {
-            menu.classList.remove('menu-opened');
-            icon.classList.remove('menu-opened');
-        }
+menuButton.onclick = function () {
+    if (open == false) {
+        icon.classList.add('menu-opened');
+        menu.classList.add('menu-opened');
+        open = !open;
     }
+    else {
+        menu.classList.add('menu-closed');
+        icon.classList.remove('menu-opened');
+        menu.classList.remove('menu-opened');
+        setTimeout(closeMenu, 1000)
+        open = !open;
+    }
+    function closeMenu() {
+        menu.classList.remove('menu-closed');
+    }   
+}
+
+
