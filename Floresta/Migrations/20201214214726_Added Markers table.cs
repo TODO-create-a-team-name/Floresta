2df5b38695809145c8d7cbc977b47914e3fe61f8
Serge <sanichace@gmail.com>
2020-12-21 23:22:12 +0200

    uncomented stuff from migrations

--- Floresta/Migrations/20201214214726_Added Markers table.cs
+++ Floresta/Migrations/20201214214726_Added Markers table.cs
@@ -6,20 +6,20 @@ namespace Floresta.Migrations
     {
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-            //migrationBuilder.CreateTable(
-            //    name: "Markers",
-            //    columns: table => new
-            //    {
-            //        Id = table.Column<int>(type: "int", nullable: false)
-            //            .Annotation("SqlServer:Identity", "1, 1"),
-            //        Title = table.Column<string>(type: "nvarchar(max)", nullable: true),
-            //        Lat = table.Column<string>(type: "nvarchar(max)", nullable: true),
-            //        Lng = table.Column<string>(type: "nvarchar(max)", nullable: true)
-            //    },
-            //    constraints: table =>
-            //    {
-            //        table.PrimaryKey("PK_Markers", x => x.Id);
-            //    });
+            migrationBuilder.CreateTable(
+                name: "Markers",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Lat = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Lng = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Markers", x => x.Id);
+                });
         }
 
         protected override void Down(MigrationBuilder migrationBuilder)
