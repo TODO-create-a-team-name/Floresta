02c96a78d6f4e402c5cdc3c488d4bd5974233257
Vasya Lesiuk <lenono526@gmail.com>
2020-12-23 16:58:09 +0200

    CRUD for markers

--- Floresta/Controllers/MapController.cs
+++ Floresta/Controllers/MapController.cs
@@ -2,6 +2,7 @@
 using Floresta.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -17,6 +18,11 @@ namespace Floresta.Controllers
             _context = context;
         }
 
+        public IActionResult Markers()
+        {
+            var list = _context.Markers.ToList();
+            return View(list);
+        }
         public IActionResult Index()
         {
             var model = new PaymentViewModel();
@@ -30,12 +36,43 @@ namespace Floresta.Controllers
             return View(model);
         }
 
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id != null)
+            {
+                var markers = await _context.Markers.FirstOrDefaultAsync(x => x.Id == id);
+                return View(markers);
+            }
+            return NotFound();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(Marker marker)
+        {
+            _context.Markers.Update(marker);
+            await _context.SaveChangesAsync();
+            return RedirectToAction("Markers");
+        }
+
         [HttpPost]
         public async Task<IActionResult> Index(Marker marker)
         {
             _context.Markers.Add(marker);
             await _context.SaveChangesAsync();
             return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var marker = _context.Markers.FirstOrDefault(x => x.Id == id);
+            if (marker != null)
+            {
+                _context.Markers.Remove(marker);
+                await _context.SaveChangesAsync();
+                return RedirectToAction("Markers");
+            }
+            return NotFound();
         }
 
         public JsonResult GetMarkers()
