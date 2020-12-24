02cfc981fc71fcd9015e1b95e52ffb951f0c0ee0
Demianchuk <vanohahj5555@gmail.com>
2020-12-24 18:29:06 +0200

    Closed#{8} Added basic blocks to the home page and added a plant tree tree button

--- Floresta/wwwroot/js/site.js
+++ Floresta/wwwroot/js/site.js
@@ -27,36 +27,6 @@ menuButton.addEventListener('click', {
 });
 
 
-google.charts.load("current", { packages: ["corechart"] });
-google.charts.setOnLoadCallback(drawChart);
-function drawChart() {
-    var data = google.visualization.arrayToDataTable([
-        ['Trees', 'Plant'],
-        ['Посаджено', 1220],
-        ['Залишилось посадити', 5780]     
-    ]);
-
-    var options = {
-        colors: ['819F00', 'ABCD2D'],
-        pieSliceTextStyle: {
-            color: 'white',
-        },
-        fontSize: 18,
-        legend: 'none',
-        pieSliceText: 'value',
-        backgroundColor: 'none',
-        tooltip: { trigger: 'none' },
-        chartArea: {left:20,top:0,width:'100%',height:'100%'},
-    };
-
-
-    var chart = new google.visualization.PieChart(document.getElementById('donut_single'));
-    chart.draw(data, options);
-}
-
-
-
-
 // home map
 let map;
 function initMap() {
@@ -88,20 +58,3 @@ regionBt.forEach(region => {
         }
     });
 });
-menuButton.onclick = function () {
-    if (open == false) {
-        icon.classList.add('menu-opened');
-        menu.classList.add('menu-opened');
-        open = !open;
-    }
-    else {
-        menu.classList.add('menu-closed');
-        icon.classList.remove('menu-opened');
-        menu.classList.remove('menu-opened');
-        setTimeout(closeMenu, 490) 
-    }
-    function closeMenu() {
-        menu.classList.remove('menu-closed');
-        open = !open;
-    }   
-}
