43ddd3442cbe4ba4e1efd8e8836d96b678b0c1c0
Serge <sanichace@gmail.com>
2020-12-25 18:29:26 +0200

    changed redirection in SendEmail method of PaymentController

--- Floresta/Controllers/PaymentController.cs
+++ Floresta/Controllers/PaymentController.cs
@@ -95,7 +95,7 @@ namespace Floresta.Controllers
 
                 await emailService.SendEmailAsync(user.Email, "Purchase status",
                     model.Message);
-                return RedirectToAction("Purchases", "Home");
+                return RedirectToAction("Purchases", "Admin_Home");
             }
             return NotFound();
         }
