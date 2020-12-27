c1cb8049afac3ef1c8971d8b03e5b68e87545800
Vasya Lesiuk <lenono526@gmail.com>
2020-12-27 17:36:04 +0200

    Added news crud

---
+++ Floresta/Migrations/20201227120559_change news model #2.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Floresta.Migrations
+{
+    public partial class changenewsmodel2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ImageByte",
+                table: "News");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<byte[]>(
+                name: "ImageByte",
+                table: "News",
+                type: "varbinary(max)",
+                nullable: true);
+        }
+    }
+}
