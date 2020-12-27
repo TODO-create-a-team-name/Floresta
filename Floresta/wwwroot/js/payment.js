3b0c64453efe7752da3777641890be680d9c8d23
Serge <sanichace@gmail.com>
2020-12-27 22:40:49 +0200

    added popup message after purchase

--- Floresta/wwwroot/js/payment.js
+++ Floresta/wwwroot/js/payment.js
@@ -1,1 +1,5 @@
-﻿
+﻿var payButton = document.getElementById("PayButton");
+
+payButton.addEventListener('click', () => {
+    swal("Ура!", "Зовсім скоро статус оплати зв'явиться у Вас на пошті", "success");
+});
