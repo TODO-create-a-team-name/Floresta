6cd8fe0f4b42d7d6f325512017b8f59834b38f5d
Serge <sanichace@gmail.com>
2021-01-03 21:37:59 +0200

    minor changes

--- Floresta/ViewModels/LoginViewModel.cs
+++ Floresta/ViewModels/LoginViewModel.cs
@@ -11,6 +11,7 @@ namespace Floresta.ViewModels
         [Required]
         [DataType(DataType.Password)]
         public string Password { get; set; }
+
         public bool RememberMe { get; set; }
 
         public string ReturnUrl { get; set; }
